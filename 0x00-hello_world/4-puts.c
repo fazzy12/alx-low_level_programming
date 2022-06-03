c46fe69f594d4b1f0e6e4a5a5fed219fa0ad57b4
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-02 20:26:50 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multiple puzzle");
+
+	return (0);
+}	
