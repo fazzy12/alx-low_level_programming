b4ec3646da8cda2aaf6ad4990e18069c69c3bfd0
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-09 05:43:00 -0700

    check for super alphabet

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
+ *_isupper - checks for uppercase character
  *
- * Return: returns 1 if c is uppercase returns 0 otherwise
+ *@c: character to be checked
+ *
+ *Return: returns 1 if c is uppercase returns 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' || c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
