501aabefc8ceb15fbb865b7918581936a56a1270
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-09 21:31:17 -0700

    print fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include  <stdio.h>
+
+/**
+ * main - the main prints fizzbuzz
+ *
+ * Return: Alaways retrun 0
+ */
+
+int main(void)
+{
+	int i, n = 100;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (n == 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
