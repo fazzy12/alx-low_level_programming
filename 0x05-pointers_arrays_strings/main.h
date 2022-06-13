548a89a9a6b138ec5da1d51bd26c23b1657cb8b8
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-13 10:53:53 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+void reset_to_98(int *n);
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
+
