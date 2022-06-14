23eec9e1dee79b863971699c7983263a563b5e6e
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-13 17:05:12 -0700

    reverse character

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int  len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char t;
+
+	while (i < j)
+	{
+		t = s[i];
+		s[i] = s[j];
+		s[j] = t;
+		i++, j--;
+	}
+}
