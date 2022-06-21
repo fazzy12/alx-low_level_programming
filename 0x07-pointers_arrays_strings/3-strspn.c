daf745c36fa8ff32451cb62819422bc8e945e691
fazzy12 <kaluifeanyi00@gmail.com>
2022-06-20 17:09:50 -0700

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int ind = 0;
+
+	while (*s)
+	{
+		while (accept[ind])
+		{
+			if (*s == accept[ind])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[ind + 1] == '\0')
+				return (bytes);
+			ind++;
+		}
+		s++;
+	}
+
+	return (bytes);
+}
