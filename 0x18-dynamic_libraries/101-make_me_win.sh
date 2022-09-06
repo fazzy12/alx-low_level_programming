06c938d0ed834cedc5b283ccb62fc35853792442
ifeanyi kalu <kaluifeanyi00@gmail.com>
2022-09-06 01:58:05 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
-d52e6c18e0723f5b025a75dea19ef365  gm
+#!/bin/bash 
+ wget -P /tmp/ https://raw.github.com/Tecnophille/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so 
+ export LD_PRELOAD=/tmp/preload.so
