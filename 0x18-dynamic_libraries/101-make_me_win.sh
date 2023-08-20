727abcc55686964df26b54f0417eecd575c7ae05
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-20 03:08:19 +0100

    Implementing Ethical and Responsible Practices for Secure System Management

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash 
- wget -P /tmp/ https://raw.github.com/Tecnophille/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so 
- export LD_PRELOAD=/tmp/preload.so
+#!/bin/bash
+wget -P /tmp/ https://raw.github.com/fazzy12/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
