21cf12181a533033b0735bbf102d512518cec869
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-27 15:02:28 +0100

    Add Python solution for calculating the perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    # Initialize perimeter to 0
-    perimeter = 0
+    """Return the perimiter of an island.
 
-    # Define the directions to check neighbors (up, down, left, right)
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    The grid represents water by 0 and land by 1.
 
-    # Iterate through each cell in the grid
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            # Check if the current cell is land (grid[i][j] == 1)
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # Iterate through the four directions
-                for dx, dy in directions:
-                    ni, nj = i + dx, j + dy
-                    # Check if the neighbor is out of bounds or is water (0)
-                    if ni < 0 or ni >= len(grid) or nj < 0 or nj >= len(grid[0]) or grid[ni][nj] == 0:
-                        perimeter += 1
-
-    # Return the final perimeter
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
