1cfc549609a94d5cba63229088a26faafd25f019
fazzy12 <kaluifeanyi00@gmail.com>
2024-01-09 18:35:39 +0100

    Implement add_node_end function

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strlen - Computes the length of a string.
+ * @str: The string to compute the length of.
+ *
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *str)
+{
+    size_t len = 0;
+
+    while (str[len])
+        len++;
+
+    return (len);
+}
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to the head of the list.
+ * @str: String to be duplicated and added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node, *last_node = *head;
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = _strlen(str);
+
+    new_node->next = NULL;
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    while (last_node->next != NULL)
+        last_node = last_node->next;
+
+    last_node->next = new_node;
+
+    return (new_node);
+}
