2fd9d3c544393a933b2187f82b4717002aadcf17
fazzy12 <kaluifeanyi00@gmail.com>
2024-01-09 13:39:32 +0100

    Add print_list function for singly linked list traversal

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
  *
- * Return: the number of nodes
+ * Return: the number of nodess
  */
 
 size_t print_list(const list_t *h)
