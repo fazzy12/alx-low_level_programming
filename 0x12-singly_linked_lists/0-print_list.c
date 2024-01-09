72d87f55d4ed903d1cecf6d31293d69facdd0c91
fazzy12 <kaluifeanyi00@gmail.com>
2024-01-09 12:58:19 +0100

    Add print_list function for singly linked list traversal

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,25 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-
-        h = h->next;
-        count++;
-    }
-    return (count);
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
